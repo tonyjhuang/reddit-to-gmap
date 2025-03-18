4c43cc4d58286e7ef64de354ddc0ffaf94eb9b79
Tony Huang <tony.huang.jun@gmail.com>
2025-03-18 11:01:16 +0900

    fix cache path

---
+++ cache/cache.go
@@ -0,0 +1,65 @@
+package cache
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const cacheDir = ".cache"
+
+type Cache struct {
+	Posts interface{} `json:"posts"`
+}
+
+func EnsureCacheDir() error {
+	if err := os.MkdirAll(cacheDir, 0755); err != nil {
+		return fmt.Errorf("error creating cache directory: %v", err)
+	}
+	return nil
+}
+
+func GetCachePath(subreddit string) string {
+	return filepath.Join(cacheDir, fmt.Sprintf("%s.json", subreddit))
+}
+
+func WriteToCache(subreddit string, data interface{}) error {
+	if err := EnsureCacheDir(); err != nil {
+		return err
+	}
+
+	cache := Cache{
+		Posts: data,
+	}
+
+	file, err := json.MarshalIndent(cache, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshaling cache data: %v", err)
+	}
+
+	if err := os.WriteFile(GetCachePath(subreddit), file, 0644); err != nil {
+		return fmt.Errorf("error writing cache file: %v", err)
+	}
+
+	return nil
+}
+
+func ReadFromCache(subreddit string) (*Cache, error) {
+	file, err := os.ReadFile(GetCachePath(subreddit))
+	if err != nil {
+		return nil, fmt.Errorf("error reading cache file: %v", err)
+	}
+
+	var cache Cache
+	if err := json.Unmarshal(file, &cache); err != nil {
+		return nil, fmt.Errorf("error unmarshaling cache data: %v", err)
+	}
+
+	return &cache, nil
+}
+
+func CacheExists(subreddit string) bool {
+	_, err := os.Stat(GetCachePath(subreddit))
+	return err == nil
+} 
